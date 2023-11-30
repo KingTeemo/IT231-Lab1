4db2aaf9fce8b83d64bd007f9046791bde0e7020
KingTeemo <ewatt1023@gmail.com>
2023-11-30 10:54:11 -0500

    step_5

---
+++ program/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Sadly, no counter for the Jigma");
+        }
+    }
+}
